a3890e465c3af4d9faadea9d1539ae48299d9cb1
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2022-05-17 18:54:06 +0200

    Create Main.java

---
+++ Bomberman/src/sk/uniza/fri/Main.java
@@ -0,0 +1,15 @@
+package sk.uniza.fri;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Fíla
+ * Date: 27. 4. 2022
+ * Time: 23:03
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        Canvas canvas = new Canvas("Bomberman", 690, 630);
+        Bomberman bomberman = new Bomberman();
+    }
+}
