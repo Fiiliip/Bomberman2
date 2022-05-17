2fd950cbaa1f98e4c70cda309c2f46e9c223a847
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2022-05-17 18:54:35 +0200

    Create Ballom.java

---
+++ Bomberman/src/sk/uniza/fri/characters/enemies/Ballom.java
@@ -0,0 +1,45 @@
+package sk.uniza.fri.characters.enemies;
+
+/**
+ * 27. 4. 2022 - 23:03
+ *
+ * @author Fíla
+ */
+public class Ballom extends Enemy {
+
+    public Ballom(String fileWithTexture, int x, int y) {
+        super(fileWithTexture, x, y);
+
+        this.setNumberOfLives(1);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
