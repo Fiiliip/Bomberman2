da4103c7db01826be50761eeb955e87ba4c56ed0
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2022-05-17 18:54:27 +0200

    Create Canvas.java

---
+++ Bomberman/src/sk/uniza/fri/gui/Canvas.java
@@ -0,0 +1,38 @@
+package sk.uniza.fri.gui;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Canvas {
+
+    private static Canvas platno;
+    private JFrame canvas;
+
+    private ArrayList<Object> objects;
+
+    public Canvas(String title, int width, int height) {
+        this.canvas = new JFrame(title);
+        this.canvas.setSize(width, height);
+        this.canvas.setVisible(true);
+
+        this.objects = new ArrayList<Object>();
+
+        platno = this;
+//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    }
+
+    public static Canvas getCanvas() {
+        return platno;
+    }
+
+    public void redraw() {
+        this.canvas.repaint();
+        // TODO: tu mi dačo chýba, ale ešte neviem čo
+    }
+
+    public void draw(Object object, BufferedImage image) {
+        this.objects.add(object);
+        this.redraw();
+    }
+}
