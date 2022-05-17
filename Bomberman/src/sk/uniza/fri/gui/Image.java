460dc723744c8a964da76ca4dd32b784f8b02eeb
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2022-05-17 18:54:22 +0200

    Create Image.java

---
+++ Bomberman/src/sk/uniza/fri/gui/Image.java
@@ -0,0 +1,36 @@
+package sk.uniza.fri.gui;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Image {
+
+    private BufferedImage image;
+
+    public Image(String fileWithTexture) {
+        this.image = this.loadImageFromFile(fileWithTexture);
+    }
+
+    public int getWidth() {
+        return this.image.getWidth();
+    }
+
+    public int getHeight() {
+        return this.image.getHeight();
+    }
+
+    private BufferedImage loadImageFromFile(String fileWithTexture) {
+        BufferedImage loadedImage = null;
+        try {
+            loadedImage = ImageIO.read(new File(fileWithTexture));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Canvas.getCanvas().draw(this, loadedImage);
+
+        return loadedImage;
+    }
+}
