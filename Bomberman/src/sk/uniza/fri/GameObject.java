7b50f75664ef4cd3730e4ba0ea8b64e792432e8e
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2022-05-21 14:11:02 +0200

    Added more options for setting textures.

--- Bomberman/src/sk/uniza/fri/GameObject.java
+++ Bomberman/src/sk/uniza/fri/GameObject.java
@@ -13,8 +13,13 @@ public abstract class GameObject {
 
     private Obrazok texture;
 
-    private int row;
-    private int column;
+    protected int row;
+    protected int column;
+
+    public GameObject(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 
     public GameObject(BufferedImage texture, int row, int column) {
         this.texture = new Obrazok(texture);
@@ -22,8 +27,34 @@ public abstract class GameObject {
         this.row = row;
         this.column = column;
 
-        this.texture.setPosition(column * 30, row * 30 + 30);
+        this.texture.setPosition(this.column * 30, this.row * 30 + 30);
         this.texture.zobraz();
+    }
+
+    public void setTexture(String fileWithTexture) {
+        if (this.texture == null) {
+            this.texture = new Obrazok(fileWithTexture);
+            this.texture.zobraz();
+        } else {
+            this.texture.zmenObrazok(fileWithTexture);
+        }
+
+        this.texture.setPosition(this.column * 30, this.row * 30 + 30);
+    }
+
+    public void setTexture(BufferedImage bufferedImage) {
+        if (this.texture == null) {
+            this.texture = new Obrazok(bufferedImage);
+            this.texture.zobraz();
+        } else {
+            this.texture.zmenObrazok(bufferedImage);
+        }
+
+        this.texture.setPosition(this.column * 30, this.row * 30 + 30);
+    }
+
+    public Obrazok getTexture() {
+        return this.texture;
     }
 
     public int getRow() {
@@ -46,14 +77,6 @@ public abstract class GameObject {
 
     public void rotateTexture(int angle) {
         this.texture.zmenUhol(angle);
-    }
-
-    public void setTexture(String fileWithTexture) {
-        this.texture.zmenObrazok(fileWithTexture);
-    }
-
-    public void setTexture(BufferedImage bufferedImage) {
-        this.texture.zmenObrazok(bufferedImage);
     }
 
     public void showTexture() {
